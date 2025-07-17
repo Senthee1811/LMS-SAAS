05fcfe164eaa7e31b752e80df07f9714f61630cd
Senthee1811 <esentheepan@gmail.com>
2025-07-17 22:00:36 +0530

    implement UI startup

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,40 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import CompanionCard from '@/components/CompanionCard';
+import CompanionsList from '@/components/CompanionsList';
+import CTA from '@/components/CTA';
 
 const Page = () => {
   return (
     <div>
-      <h1 className='text-2xl underline'>Welcome to SAAS</h1>
-      <Button >
-        Let's get Started
-      </Button>
+      <h1>Popular Companions</h1>
+     <section className='home-section'>
+      <CompanionCard
+      id='123'
+      name='Neura the Brainy Explorer'
+      topic='DDDD'
+      subject="science"
+      duration = {45} 
+      color='#ffda6e'
+      />
+      <CompanionCard
+      id='123'
+      name='Countsy the Number Wizard'
+      subject="science"
+      topic='Derivatives & Intertnals'
+      duration = {30} 
+      color='#e5d0ff'/>
+      <CompanionCard
+      id='789'
+      name='Verba the Vocabulary Builder'
+      subject="English Literature"
+      topic='language'
+      duration = {45} 
+      color='#BDE7FF'/>
+      
+     </section>
+     <section className='home-section'>
+      <CompanionsList/>
+      <CTA/>
+     </section>
     </div>
   )
 }
