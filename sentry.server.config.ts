6468cf7c4fec1b41852241f178a7ff6f3f7e684f
Senthee1811 <esentheepan@gmail.com>
2025-07-19 23:18:27 +0530

    Voice & Db Integration Done

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://cc5f535f507d1ee78e7c7119dbbdd351@o4509684306935808.ingest.de.sentry.io/4509696382730320",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
